26646f84e58bd5bc3ff9ade7c76d714bfc12e71f
aleleotta <aleleotta@106-02.info2>
2022-11-29 12:22:42 +0100

    .

--- Functions Topic 4/src/exercises/Exercise8.java
+++ Functions Topic 4/src/exercises/Exercise8.java
@@ -3,8 +3,34 @@ import java.util.*;
 
 public class Exercise8 {
 	public static void main(String[] args) {
+		double a, b, option;
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce the first number: ");
+		a = sc.nextDouble();
+		System.out.println("Introduce the second number: ");
+		b = sc.nextDouble();
+		System.out.println("Operators:\n'+' = 1\n'-' = 2\n'*' = 3\n'/' = 4 ");
+		System.out.print("Choose an operator: ");
+		option = sc.nextDouble();
+		double result = calculator(option, a, b);
+		System.out.println("Result: " + result);
 		sc.close();
+	}
+	
+	static double calculator(double option, double a, double b){
+		double result = 0;
+		if(option == 1) {
+			result = a + b;
+		} else if(option == 2) {
+			result = a - b;
+		} else if(option == 3) {
+			result = a * b;
+		} else if(option == 4) {
+			result = a / b;
+		} else {
+			System.out.println("Invalid operator!");
+		}
+		return result;
 	}
 }
 /*Diseñar la función calculadora(),
